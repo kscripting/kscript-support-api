47e06ae2ef239924b4daffd57ce04f24a73522d9
Holger Brandl <holgerbrandl@gmail.com>
2017-05-02 22:44:28 +0200

    added incubator package for devel

---
+++ src/main/kotlin/kscript/experimental/Incubator.kt
@@ -0,0 +1,20 @@
+package kscript.experimental
+
+import java.io.File
+
+/**
+ * Experimental support methods. Those may change without further notice.
+ *
+ * @author Holger Brandl
+ */
+
+
+fun <T> File.mapLines(trafo: (String) -> T) {
+    return useLines {
+        it.map { trafo(it) }
+    }
+}
+
+fun String.processLines(trafo: (String) -> String) {
+    split("\n").map { println(trafo(it)) }
+}
