f5bc59af25f5aca55dd586d688553950d44c973f
Holger Brandl <holgerbrandl@gmail.com>
2017-05-02 15:46:28 +0200

    inc version of kotlintest

--- src/test/kotlin/kscript/test/SupportApiTest.kt
+++ src/test/kotlin/kscript/test/SupportApiTest.kt
@@ -1,16 +1,21 @@
 package kscript.test
 
-import io.kotlintest.specs.FlatSpec
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
 import kscript.print
 import kscript.stdin
 
 /**
  * @author Holger Brandl
  */
-class SupportApiTest : FlatSpec() { init {
+class SupportApiTest : StringSpec() { init {
 
-    "it" should "allow to use stdin for filtering and mapping" {
-        val longDf = stdin.filter { "^de0[-0]*".toRegex().matches(it) }.map { it + "foo:" }.print()
+    "length should return size of string" {
+        "hello".length shouldBe 5
+    }
+
+    "allow to use stdin for filtering and mapping" {
+        stdin.filter { "^de0[-0]*".toRegex().matches(it) }.map { it + "foo:" }.print()
     }
 }
 }
